b7aa04fe0895f165ef80bc3956915520fe39624d
vaniusha3 <dj_vaniusha@mail.ru>
2016-03-27 11:28:18 +0300

    Update astra_install.sh

--- astra_install.sh
+++ astra_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Se Instaleaza Astra"
-wget -q http://dev.cesbo.com/download/astra/4.4.182/extended/x86/linux-64bit/astra > /dev/null
+wget -q http://dev.cesbo.com/download/astra/5.62-rc9/x86/linux-64bit/astra > /dev/null
 chmod +x astra > /dev/null
 sudo mv astra /usr/bin/ > /dev/null
 sudo mkdir /etc/astra > /dev/null
